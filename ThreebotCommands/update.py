800694cb59800548a6d0a4044c42aab2aa89c791
alkovalic <alkovalic@gmail.com>
2017-07-30 03:11:44 -0500

    Test commit.

---
+++ ThreebotCommands/update.py
@@ -0,0 +1,19 @@
+import Command
+import git
+import os
+import subprocess
+
+
+class UPDATE(Command.Command):
+
+    async def run(self, client, message):
+
+        await client.send_message(message.channel, "Updating Threebot..")
+
+        g = git.cmd.Git(os.path.dirname(os.path.dirname(__file__)))
+        g.pull()
+
+        command = "/usr/bin/sudo /sbin/shutdown -r now"
+        process = subprocess.Popen(command.split(), stdout=subprocess.PIPE)
+        output = process.communicate()[0]
+        print(output)
