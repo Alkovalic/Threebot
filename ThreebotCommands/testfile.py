b53f90f7448393c2d5008755fc48d8e8d67b7ece
alkovalic <alkovalic@gmail.com>
2017-07-30 02:46:57 -0500

    Test commit.

--- ThreebotCommands/testfile.py
+++ ThreebotCommands/testfile.py
@@ -4,4 +4,4 @@ import Command
 class MEME(Command.Command):
 
     async def run(self, client, message):
-        return await client.send_message("im gay")
+        return await client.send_message(message.channel, "im gay")
