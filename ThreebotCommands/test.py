b4d4674a377a518f30481e22be89a80db1fb4622
alkovalic <alkovalic@gmail.com>
2017-09-03 15:40:00 -0500

    Attempting video embedding.

---
+++ ThreebotCommands/test.py
@@ -0,0 +1,11 @@
+import Command
+import discord
+
+
+class TEST(Command.Command):
+
+    async def run(self, client, message):
+
+        embed = discord.Embed(title='<video id="sampleMovie" src="HTML5Sample.mov"></video>')
+
+        return await client.send_message(embed=embed)
